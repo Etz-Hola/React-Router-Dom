7d4fe0ee9df92009143c0073c4dbe85c06d3424e
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-10-31 16:13:20 +0100

    update

--- src/App.js
+++ src/App.js
@@ -12,8 +12,35 @@ import Footer from './Footer'
 const App = () => {
 
   const [search, setSearch] = useState('')
-  const [post, setPost] = useState()
-  const [seatchResult, setSearchResult] = useState()
+  const [post, setPost] = useState([
+    {
+      id:1,
+      title: 'Rocco',
+      date: 'July 01, 2021 11:17:36 AM',
+      body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum laborum, debitis fugit necessitatibus minima molestiae. Vero natus eaque excepturi dolor dignissimos numquam voluptatibus dolorum provident.'
+    },
+    {
+      id:2,
+      title: 'Ola',
+      date: 'July 19, 2024 04:11:36 PM',
+      body: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, aspernatur!'
+    },
+    {
+      id:3,
+      title: 'Kanas',
+      date: 'September 28, 2021 01:17:36 AM',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis voluptates incidunt inventore doloribus!'
+    },
+    {
+      id:4,
+      title: 'Muaz',
+      date: 'August 17, 2020 06:55:55 PM',
+      body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis voluptates incidunt inventore doloribus?'
+    }
+
+  ])
+  const [searchResult, setSearchResult] = useState([])
+
   return (
     <div className='App'>
       <Header title='DLT Student Blog'/>
