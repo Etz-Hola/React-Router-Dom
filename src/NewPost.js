47d750dce53e328339ca16926ca727bcdc96e2a2
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-01 14:31:47 +0100

    update

--- src/NewPost.js
+++ src/NewPost.js
@@ -1,9 +1,16 @@
-const NewPost = ({posTitle, setPostTitle})
+const NewPost = ({ postTitle, setBody, setPostBody, SetPostTitle, handleSubmit }) => {
+    return (
+      <main className="NewPost">
+        <h2>New Post</h2>
+        <form className="newPostForm" onSubmit={handleSubmit}>
+          <label htmlFor="postTitle">Post Title</label>
+          <input id="postTitle" type="text" required value={postTitle} onChange={(e.target.value)}/>
+          <label htmlFor="postBody">Post Body</label>
+          <textarea name="postBody" id="postBody"></textarea>
 
-const NewPost = () => {
-  return (
-    <div>NewPost</div>
-  )
+        </form>
+      </main>
+    )
 }
 
 export default NewPost
