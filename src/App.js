d519155647fcb547a66d5044b0c64e1a5bae53e1
Qadir30 <141998682+Qadir30@users.noreply.github.com>
2023-11-01 15:47:54 +0100

    update

--- src/App.js
+++ src/App.js
@@ -15,25 +15,25 @@ const App = () => {
   const [posts, setPosts] = useState([
     {
       id:1,
-      title: 'Rocco',
+      title: 'Adeshola',
       date: 'July 01, 2021 11:17:36 AM',
       body: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum laborum, debitis fugit necessitatibus minima molestiae. Vero natus eaque excepturi dolor dignissimos numquam voluptatibus dolorum provident.'
     },
     {
       id:2,
-      title: 'Ola',
+      title: 'Adesoji',
       date: 'July 19, 2024 04:11:36 PM',
       body: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, aspernatur!'
     },
     {
       id:3,
-      title: 'Kanas',
+      title: 'Adesoye',
       date: 'September 28, 2021 01:17:36 AM',
       body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis voluptates incidunt inventore doloribus!'
     },
     {
       id:4,
-      title: 'Muaz',
+      title: 'Olalekan',
       date: 'August 17, 2020 06:55:55 PM',
       body: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis voluptates incidunt inventore doloribus?'
     }
